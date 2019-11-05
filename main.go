4121da1b2fecb1c8ddac602e468ae4655660322f
Carlos Souza <carloshrsouza@gmail.com>
2019-11-05 14:11:38 -0500

    Fix main

--- main.go
+++ main.go
@@ -6,6 +6,22 @@ import (
 	"time"
 )
 
+var months = []time.Month{
+	time.January,
+	time.February,
+	time.March,
+	time.April,
+	time.May,
+	time.June,
+	time.July,
+	time.August,
+	time.September,
+	time.October,
+	time.November,
+	time.December,
+	time.January,
+}
+
 func main() {
 	bu, _ := m2.CreateBudget(time.January, 1000)
 	bu.AddItem("bananas", 10.0)
@@ -13,7 +29,9 @@ func main() {
 	fmt.Println("Items in January:", len(bu.Items))
 	fmt.Printf("Current cost for January: $%.2f \n", bu.CurrentCost())
 
-	bu2, _ := m2.CreateBudget(time.February, 1000)
+	m2.CreateBudget(time.February, 1000)
+
+	bu2 := m2.GetBudget(time.February)
 	bu2.AddItem("bananas", 10.0)
 	bu2.AddItem("coffee", 3.99)
 	bu2.AddItem("gym", 50.0)
@@ -23,22 +41,6 @@ func main() {
 
 	fmt.Println("Resetting Budget Report...")
 	m2.InitializeReport()
-
-	var months = []time.Month{
-		time.January,
-		time.January,
-		time.February,
-		time.March,
-		time.April,
-		time.May,
-		time.June,
-		time.July,
-		time.August,
-		time.September,
-		time.October,
-		time.November,
-		time.December,
-	}
 
 	for _, month := range months {
 		_, err := m2.CreateBudget(month, 100.00)
